4393b777eeb6b6facb9bc8baedb568adf9e4a400
ahubiak <alyssa.hubiak@gmail.com>
2022-09-28 23:47:14 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+from flask import Flask, request,render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = 'unavailable'
+    if prediction == 0:
+        output = 'healthy'
+    elif prediction == 1:
+        output = 'sick'
+
+    return render_template('index.html', prediction_text='Illness status is {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
